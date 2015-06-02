7978769e06533d6bc26e97059e29da53f94b9036
Jacky Liang <bluelegopiece@hotmail.com>
2015-06-02 13:08:29 -0400

    added file existence check

--- tf2op.py
+++ tf2op.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
-import requests,json
+import requests,json,os.path,sys
 
 # Constants
 tf2op_url = "http://www.tf2outpost.com/api/core"
 tf2op_db = "tradeid.json"
 
 # Parse tradeid.json
-with open(tf2op_db) as data_file:    
-    trades = json.load(data_file)
+if os.path.exists(tf2op_db):
+    with open(tf2op_db) as data_file:    
+        trades = json.load(data_file)
+else:
+    print "Error - please create a tradeid.json file!"
+    sys.exit(0)
 
 # Perform a bump on each trade
 for trade in trades:
